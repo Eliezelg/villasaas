ca81c9146791f368889b42d709f0bfec6b236ca3
eliezer <eliezelg@gmail.com>
2025-07-15 01:51:08 +0300

    fssfsdjbkhvgbs

--- villa-saas/packages/i18n/dist/index.js
+++ villa-saas/packages/i18n/dist/index.js
@@ -1,79 +1,91 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.messages = exports.localeNames = exports.defaultLocale = exports.locales = void 0;
+exports.getLocaleFromAcceptLanguage = getLocaleFromAcceptLanguage;
+exports.getLocaleFromPath = getLocaleFromPath;
+exports.formatCurrency = formatCurrency;
+exports.formatDate = formatDate;
+exports.formatNumber = formatNumber;
+exports.pluralize = pluralize;
 // French
-import frCommon from './locales/fr/common.json';
-import frBooking from './locales/fr/booking.json';
-import frEmails from './locales/fr/emails.json';
-import frAdmin from './locales/fr/admin.json';
+const common_json_1 = __importDefault(require("./locales/fr/common.json"));
+const booking_json_1 = __importDefault(require("./locales/fr/booking.json"));
+const emails_json_1 = __importDefault(require("./locales/fr/emails.json"));
+const admin_json_1 = __importDefault(require("./locales/fr/admin.json"));
 // English
-import enCommon from './locales/en/common.json';
-import enBooking from './locales/en/booking.json';
-import enEmails from './locales/en/emails.json';
-import enAdmin from './locales/en/admin.json';
+const common_json_2 = __importDefault(require("./locales/en/common.json"));
+const booking_json_2 = __importDefault(require("./locales/en/booking.json"));
+const emails_json_2 = __importDefault(require("./locales/en/emails.json"));
+const admin_json_2 = __importDefault(require("./locales/en/admin.json"));
 // Spanish
-import esCommon from './locales/es/common.json';
-import esBooking from './locales/es/booking.json';
-import esEmails from './locales/es/emails.json';
-import esAdmin from './locales/es/admin.json';
+const common_json_3 = __importDefault(require("./locales/es/common.json"));
+const booking_json_3 = __importDefault(require("./locales/es/booking.json"));
+const emails_json_3 = __importDefault(require("./locales/es/emails.json"));
+const admin_json_3 = __importDefault(require("./locales/es/admin.json"));
 // German
-import deCommon from './locales/de/common.json';
-import deBooking from './locales/de/booking.json';
-import deEmails from './locales/de/emails.json';
-import deAdmin from './locales/de/admin.json';
+const common_json_4 = __importDefault(require("./locales/de/common.json"));
+const booking_json_4 = __importDefault(require("./locales/de/booking.json"));
+const emails_json_4 = __importDefault(require("./locales/de/emails.json"));
+const admin_json_4 = __importDefault(require("./locales/de/admin.json"));
 // Italian
-import itCommon from './locales/it/common.json';
-import itBooking from './locales/it/booking.json';
-import itEmails from './locales/it/emails.json';
-import itAdmin from './locales/it/admin.json';
+const common_json_5 = __importDefault(require("./locales/it/common.json"));
+const booking_json_5 = __importDefault(require("./locales/it/booking.json"));
+const emails_json_5 = __importDefault(require("./locales/it/emails.json"));
+const admin_json_5 = __importDefault(require("./locales/it/admin.json"));
 // Portuguese
-import ptCommon from './locales/pt/common.json';
-import ptBooking from './locales/pt/booking.json';
-import ptEmails from './locales/pt/emails.json';
-import ptAdmin from './locales/pt/admin.json';
+const common_json_6 = __importDefault(require("./locales/pt/common.json"));
+const booking_json_6 = __importDefault(require("./locales/pt/booking.json"));
+const emails_json_6 = __importDefault(require("./locales/pt/emails.json"));
+const admin_json_6 = __importDefault(require("./locales/pt/admin.json"));
 // Dutch
-import nlCommon from './locales/nl/common.json';
-import nlBooking from './locales/nl/booking.json';
-import nlEmails from './locales/nl/emails.json';
-import nlAdmin from './locales/nl/admin.json';
+const common_json_7 = __importDefault(require("./locales/nl/common.json"));
+const booking_json_7 = __importDefault(require("./locales/nl/booking.json"));
+const emails_json_7 = __importDefault(require("./locales/nl/emails.json"));
+const admin_json_7 = __importDefault(require("./locales/nl/admin.json"));
 // Russian
-import ruCommon from './locales/ru/common.json';
-import ruBooking from './locales/ru/booking.json';
-import ruEmails from './locales/ru/emails.json';
-import ruAdmin from './locales/ru/admin.json';
+const common_json_8 = __importDefault(require("./locales/ru/common.json"));
+const booking_json_8 = __importDefault(require("./locales/ru/booking.json"));
+const emails_json_8 = __importDefault(require("./locales/ru/emails.json"));
+const admin_json_8 = __importDefault(require("./locales/ru/admin.json"));
 // Chinese
-import zhCommon from './locales/zh/common.json';
-import zhBooking from './locales/zh/booking.json';
-import zhEmails from './locales/zh/emails.json';
-import zhAdmin from './locales/zh/admin.json';
+const common_json_9 = __importDefault(require("./locales/zh/common.json"));
+const booking_json_9 = __importDefault(require("./locales/zh/booking.json"));
+const emails_json_9 = __importDefault(require("./locales/zh/emails.json"));
+const admin_json_9 = __importDefault(require("./locales/zh/admin.json"));
 // Japanese
-import jaCommon from './locales/ja/common.json';
-import jaBooking from './locales/ja/booking.json';
-import jaEmails from './locales/ja/emails.json';
-import jaAdmin from './locales/ja/admin.json';
+const common_json_10 = __importDefault(require("./locales/ja/common.json"));
+const booking_json_10 = __importDefault(require("./locales/ja/booking.json"));
+const emails_json_10 = __importDefault(require("./locales/ja/emails.json"));
+const admin_json_10 = __importDefault(require("./locales/ja/admin.json"));
 // Arabic
-import arCommon from './locales/ar/common.json';
-import arBooking from './locales/ar/booking.json';
-import arEmails from './locales/ar/emails.json';
-import arAdmin from './locales/ar/admin.json';
+const common_json_11 = __importDefault(require("./locales/ar/common.json"));
+const booking_json_11 = __importDefault(require("./locales/ar/booking.json"));
+const emails_json_11 = __importDefault(require("./locales/ar/emails.json"));
+const admin_json_11 = __importDefault(require("./locales/ar/admin.json"));
 // Hebrew
-import heCommon from './locales/he/common.json';
-import heBooking from './locales/he/booking.json';
-import heEmails from './locales/he/emails.json';
-import heAdmin from './locales/he/admin.json';
+const common_json_12 = __importDefault(require("./locales/he/common.json"));
+const booking_json_12 = __importDefault(require("./locales/he/booking.json"));
+const emails_json_12 = __importDefault(require("./locales/he/emails.json"));
+const admin_json_12 = __importDefault(require("./locales/he/admin.json"));
 // Hindi
-import hiCommon from './locales/hi/common.json';
-import hiBooking from './locales/hi/booking.json';
-import hiEmails from './locales/hi/emails.json';
-import hiAdmin from './locales/hi/admin.json';
+const common_json_13 = __importDefault(require("./locales/hi/common.json"));
+const booking_json_13 = __importDefault(require("./locales/hi/booking.json"));
+const emails_json_13 = __importDefault(require("./locales/hi/emails.json"));
+const admin_json_13 = __importDefault(require("./locales/hi/admin.json"));
 // Turkish
-import trCommon from './locales/tr/common.json';
-import trBooking from './locales/tr/booking.json';
-import trEmails from './locales/tr/emails.json';
-import trAdmin from './locales/tr/admin.json';
+const common_json_14 = __importDefault(require("./locales/tr/common.json"));
+const booking_json_14 = __importDefault(require("./locales/tr/booking.json"));
+const emails_json_14 = __importDefault(require("./locales/tr/emails.json"));
+const admin_json_14 = __importDefault(require("./locales/tr/admin.json"));
 // Polish
-import plCommon from './locales/pl/common.json';
-import plBooking from './locales/pl/booking.json';
-import plEmails from './locales/pl/emails.json';
-import plAdmin from './locales/pl/admin.json';
-export const locales = [
+const common_json_15 = __importDefault(require("./locales/pl/common.json"));
+const booking_json_15 = __importDefault(require("./locales/pl/booking.json"));
+const emails_json_15 = __importDefault(require("./locales/pl/emails.json"));
+const admin_json_15 = __importDefault(require("./locales/pl/admin.json"));
+exports.locales = [
     'fr', // Français
     'en', // English
     'es', // Español
@@ -90,9 +102,9 @@ export const locales = [
     'tr', // Türkçe
     'pl', // Polski
 ];
-export const defaultLocale = 'fr';
+exports.defaultLocale = 'fr';
 // Language display names
-export const localeNames = {
+exports.localeNames = {
     fr: 'Français',
     en: 'English',
     es: 'Español',
@@ -109,134 +121,134 @@ export const localeNames = {
     tr: 'Türkçe',
     pl: 'Polski',
 };
-export const messages = {
+exports.messages = {
     fr: {
-        common: frCommon,
-        booking: frBooking,
-        emails: frEmails,
-        admin: frAdmin,
+        common: common_json_1.default,
+        booking: booking_json_1.default,
+        emails: emails_json_1.default,
+        admin: admin_json_1.default,
     },
     en: {
-        common: enCommon,
-        booking: enBooking,
-        emails: enEmails,
-        admin: enAdmin,
+        common: common_json_2.default,
+        booking: booking_json_2.default,
+        emails: emails_json_2.default,
+        admin: admin_json_2.default,
     },
     es: {
-        common: esCommon,
-        booking: esBooking,
-        emails: esEmails,
-        admin: esAdmin,
+        common: common_json_3.default,
+        booking: booking_json_3.default,
+        emails: emails_json_3.default,
+        admin: admin_json_3.default,
     },
     de: {
-        common: deCommon,
-        booking: deBooking,
-        emails: deEmails,
-        admin: deAdmin,
+        common: common_json_4.default,
+        booking: booking_json_4.default,
+        emails: emails_json_4.default,
+        admin: admin_json_4.default,
     },
     it: {
-        common: itCommon,
-        booking: itBooking,
-        emails: itEmails,
-        admin: itAdmin,
+        common: common_json_5.default,
+        booking: booking_json_5.default,
+        emails: emails_json_5.default,
+        admin: admin_json_5.default,
     },
     pt: {
-        common: ptCommon,
-        booking: ptBooking,
-        emails: ptEmails,
-        admin: ptAdmin,
+        common: common_json_6.default,
+        booking: booking_json_6.default,
+        emails: emails_json_6.default,
+        admin: admin_json_6.default,
     },
     nl: {
-        common: nlCommon,
-        booking: nlBooking,
-        emails: nlEmails,
-        admin: nlAdmin,
+        common: common_json_7.default,
+        booking: booking_json_7.default,
+        emails: emails_json_7.default,
+        admin: admin_json_7.default,
     },
     ru: {
-        common: ruCommon,
-        booking: ruBooking,
-        emails: ruEmails,
-        admin: ruAdmin,
+        common: common_json_8.default,
+        booking: booking_json_8.default,
+        emails: emails_json_8.default,
+        admin: admin_json_8.default,
     },
     zh: {
-        common: zhCommon,
-        booking: zhBooking,
-        emails: zhEmails,
-        admin: zhAdmin,
+        common: common_json_9.default,
+        booking: booking_json_9.default,
+        emails: emails_json_9.default,
+        admin: admin_json_9.default,
     },
     ja: {
-        common: jaCommon,
-        booking: jaBooking,
-        emails: jaEmails,
-        admin: jaAdmin,
+        common: common_json_10.default,
+        booking: booking_json_10.default,
+        emails: emails_json_10.default,
+        admin: admin_json_10.default,
     },
     ar: {
-        common: arCommon,
-        booking: arBooking,
-        emails: arEmails,
-        admin: arAdmin,
+        common: common_json_11.default,
+        booking: booking_json_11.default,
+        emails: emails_json_11.default,
+        admin: admin_json_11.default,
     },
     he: {
-        common: heCommon,
-        booking: heBooking,
-        emails: heEmails,
-        admin: heAdmin,
+        common: common_json_12.default,
+        booking: booking_json_12.default,
+        emails: emails_json_12.default,
+        admin: admin_json_12.default,
     },
     hi: {
-        common: hiCommon,
-        booking: hiBooking,
-        emails: hiEmails,
-        admin: hiAdmin,
+        common: common_json_13.default,
+        booking: booking_json_13.default,
+        emails: emails_json_13.default,
+        admin: admin_json_13.default,
     },
     tr: {
-        common: trCommon,
-        booking: trBooking,
-        emails: trEmails,
-        admin: trAdmin,
+        common: common_json_14.default,
+        booking: booking_json_14.default,
+        emails: emails_json_14.default,
+        admin: admin_json_14.default,
     },
     pl: {
-        common: plCommon,
-        booking: plBooking,
-        emails: plEmails,
-        admin: plAdmin,
+        common: common_json_15.default,
+        booking: booking_json_15.default,
+        emails: emails_json_15.default,
+        admin: admin_json_15.default,
     },
 };
 // Helper functions
-export function getLocaleFromAcceptLanguage(acceptLanguage) {
+function getLocaleFromAcceptLanguage(acceptLanguage) {
     const languages = acceptLanguage
         .split(',')
         .map(lang => lang.split(';')[0].trim().toLowerCase());
     for (const lang of languages) {
         const locale = lang.split('-')[0];
-        if (locales.includes(locale)) {
+        if (exports.locales.includes(locale)) {
             return locale;
         }
     }
-    return defaultLocale;
+    return exports.defaultLocale;
 }
-export function getLocaleFromPath(pathname) {
+function getLocaleFromPath(pathname) {
     const segments = pathname.split('/').filter(Boolean);
     const firstSegment = segments[0];
-    if (firstSegment && locales.includes(firstSegment)) {
+    if (firstSegment && exports.locales.includes(firstSegment)) {
         return firstSegment;
     }
     return null;
 }
-export function formatCurrency(amount, locale, currency = 'EUR') {
+function formatCurrency(amount, locale, currency = 'EUR') {
     return new Intl.NumberFormat(locale, {
         style: 'currency',
         currency,
     }).format(amount);
 }
-export function formatDate(date, locale, options) {
+function formatDate(date, locale, options) {
     const dateObj = typeof date === 'string' ? new Date(date) : date;
     return new Intl.DateTimeFormat(locale, options).format(dateObj);
 }
-export function formatNumber(value, locale, options) {
+function formatNumber(value, locale, options) {
     return new Intl.NumberFormat(locale, options).format(value);
 }
 // Pluralization rules
-export function pluralize(count, locale, key) {
+function pluralize(count, locale, key) {
     // Simple pluralization for now, can be extended with more complex rules
     if (locale === 'fr') {
         return count <= 1 ? `${key}Singular` : `${key}Plural`;
